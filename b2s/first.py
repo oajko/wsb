b118bf4ff30f1c534b67a9a2c33d6481c6b46820
oajko <102440460+oajko@users.noreply.github.com>
2026-07-25 20:35:38 +0100

    added database storage

--- b2s/first.py
+++ b2s/first.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import TypedDict
+import sqlite3
 
 URL = "https://books.toscrape.com/catalogue/"
 stars_dict = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
@@ -112,7 +113,7 @@ def thread_get_links():
         list[str]: A list of book urls
     """
     all_links = []
-    page_urls = [f"{URL}page-{i}.html" for i in range(1, 51)]
+    page_urls = [f"{URL}page-{i}.html" for i in range(1, 3)]
     with ThreadPoolExecutor(max_workers = 16) as executor:
         futures = list(map(lambda x: executor.submit(get_links, x), page_urls))
         for f in as_completed(futures):
@@ -123,6 +124,28 @@ def thread_get_links():
 def main():
     links = thread_get_links()
     contents = thread_pool_scrape(links)
+
+    connection = sqlite3.connect("books.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS books (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            genre TEXT,
+            title TEXT,
+            price REAL,
+            stock_qty INTEGER,
+            stars INTEGER,
+            desc TEXT,
+            stock_bool BOOLEAN
+        )
+    """)
+
+    cursor.executemany("""
+        INSERT INTO books (genre, title, price, stock_qty, stars, desc, stock_bool) VALUES (?, ?, ?, ?, ?, ?, ?)""",
+        [(c['genre'], c['title'], c['price'], c['stock_qty'], c['stars'], c['desc'], c['stock_bool']) for c in contents]
+    )
+    connection.commit()
 
 if __name__ == "__main__":
     main()
